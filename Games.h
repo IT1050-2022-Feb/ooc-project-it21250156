95d4d504dea15db86aeec3ff3e905d69323ccd40
IT21277122 <99312207+MalikaIT21277122@users.noreply.github.com>
2022-05-18 17:08:28 +0000

    Games

---
+++ Games.h
@@ -0,0 +1,16 @@
+class Games
+{
+private:
+	int gameId;
+	char gameName[20];
+	char category[20];
+	float price;
+	
+
+public:
+	Games();
+	Games(int gId, char gName, char categ, float gPrice )
+	void updateGameDetails();
+	void storeGames();
+	~Games();
+};
